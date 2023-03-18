bddcc8d741fc5965305221927a096eeab1fe47c4
Sumedh Joshi <49696606+Sumedh-Joshi123@users.noreply.github.com>
2023-03-17 20:12:41 -0700

    Add files via upload

---
+++ data_to_dynamodb.py
@@ -0,0 +1,22 @@
+!pip install boto3
+
+import boto3
+import json
+
+access_key=""
+secret_access_key=""
+
+session=boto3.Session(aws_access_key_id=access_key,aws_secret_access_key=secret_access_key, region_name='')
+client_dynamo=session.resource('dynamodb')
+table=client_dynamo.Table('')
+
+records=""
+with open('student_data.json','r') as datafile:
+  records=json.load(datafile)
+
+count=0
+for i in records:
+  i['id']=count
+  print(i)
+  response=table.put_item(Item=i)
+  count+=1
